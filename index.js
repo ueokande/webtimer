b7868154178d04c0c2d2a50509bd944444c6dd75
Shin'ya Ueoka <ueokande@i-beam.org>
2019-04-10 19:57:21 +0900

    Fix time format

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const backgroundImage = (value, colorBar, colorBack) => {
 }
 
 const formatTime = (millis) => {
-  let min = Math.floor(millis / 1000 / 60)
-  let sec = Math.ceil(millis / 1000 % 60)
+  let min = Math.floor(Math.ceil(millis / 1000) / 60)
+  let sec = Math.ceil(millis / 1000) % 60
   if (sec < 10) {
     return `${min}:0${sec}`;
   } else {
