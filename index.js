0a5dfa7d16296b7364ce23d361aa3d9b3e18ff16
Shin'ya Ueoka <ueokande@i-beam.org>
2019-04-10 20:13:11 +0900

    Clean JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const colorAlert = '#FF70A6';
-const colorNormal = '#70D6FF'
+let params = {
+  max: 600000,
+  color1: '#70D6FF',
+  color2:  '#FF70A6',
+};
 
-const backgroundImage = (value, colorBar, colorBack) => {
+const initParams = () => {
+  for (let kv of location.search.slice(1).split('&')) {
+    let [key, value] = kv.split('=');
+    if (key === 'timer') {
+      let v = Number(value);
+      if (!isNaN(v)) {
+        params.max = v;
+      }
+    }
+  }
+};
+
+const backgroundImage = (value, colorBar) => {
+  let bgColor = '#E0E0E0';
   if (value < .5) {
-    let nextdeg = 90 + value * 360;
-    return `linear-gradient(90deg, ${colorBack} 50%, transparent 50%, transparent),
- linear-gradient(${nextdeg}deg, ${colorBar} 50%, ${colorBack} 50%, ${colorBack})`;
+    let deg = 90 + value * 360;
+    return `linear-gradient(90deg, ${bgColor} 50%, transparent 50%, transparent),
+linear-gradient(${deg}deg, ${colorBar} 50%, ${bgColor} 50%, ${bgColor})`;
 
   } else {
-    let nextdeg = -90 + 360 * (value - .5);
-    return `linear-gradient(${nextdeg}deg, ${colorBar} 50%, transparent 50%, transparent),
- linear-gradient(270deg, ${colorBar} 50%, ${colorBack} 50%, ${colorBack})`;
+    let deg = -90 + 360 * (value - .5);
+    return `linear-gradient(${deg}deg, ${colorBar} 50%, transparent 50%, transparent),
+linear-gradient(270deg, ${colorBar} 50%, ${bgColor} 50%, ${bgColor})`;
   }
 }
 
@@ -22,23 +38,6 @@ const formatTime = (millis) => {
   } else {
     return `${min}:${sec}`;
   }
-}
-
-const timerParamValue = () => {
-  let params = location.search.split('?');
-  for (let kv of params) {
-    let [key, value] = kv.split('=');
-    if (key === 'timer') {
-      return Number(value);
-    }
-  }
-  return NaN;
-}
-
-let maxMillis = 10 * 60 * 1000;
-let paramValue = timerParamValue();
-if (!isNaN(paramValue)) {
-  maxMillis = paramValue;
 }
 
 const updateFavicon = (value, color) => {
@@ -62,32 +61,38 @@ const updateFavicon = (value, color) => {
   favicon.href = canvas.toDataURL('image/png');
 };
 
+const updateBar = (value, color) => {
+  let div = document.querySelector('#timer');
+  div.style.backgroundImage = backgroundImage(value, color);
+}
+
 let startTime = Date.now() - 1;
 
 const loop = () => {
-  let spent = Math.min(Date.now() - startTime, maxMillis);
-  let value = 1 - spent / maxMillis;
+  let spent = Math.min(Date.now() - startTime, params.max);
+  let value = 1 - spent / params.max;
 
-  let textCurrent = formatTime(maxMillis - spent);
-  let textMax = formatTime(maxMillis);
+  let textCurrent = formatTime(params.max - spent);
+  let textMax = formatTime(params.max);
 
   document.title = `${textCurrent}/${textMax}`
   document.querySelector('#text-current').textContent = textCurrent;
   document.querySelector('#text-max').textContent = textMax;
 
-  let div = document.querySelector('#timer');
-  if (value < .2) {
-    div.style.backgroundImage = backgroundImage(value, colorAlert, '#E0E0E0');
-    updateFavicon(value, colorAlert);
+  if (value > .2) {
+    updateBar(value, params.color1);
+    updateFavicon(value, params.color1);
   } else {
-    div.style.backgroundImage = backgroundImage(value, colorNormal, '#E0E0E0');
-    updateFavicon(value, colorNormal);
+    updateBar(value, params.color2);
+    updateFavicon(value, params.color2);
   }
 
-  if (spent > maxMillis) {
+  if (spent > params.max) {
     return;
   }
 
   window.requestAnimationFrame(loop);
 };
+
+initParams();
 loop();
